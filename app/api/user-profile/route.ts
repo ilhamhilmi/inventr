a5c89048dbd69d44ec31fcd0eb683864b01f9609
ilhamhlmi <ilhamhilmi206@gmail.com>
2026-05-31 19:51:28 +0700

    user profile (read) day 3

---
+++ app/api/user-profile/route.ts
@@ -0,0 +1,43 @@
+import { db } from "@/lib/db";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+    try{
+        const cookieStore = await cookies();
+        const userId = cookieStore.get("user_id")
+
+        if(!userId){
+            return NextResponse.json(
+                {message: "Belum Login"},
+                {status: 401}
+            )
+        }
+
+        const [rows]: any = await db.query(
+            "SELECT username, password FROM users WHERE id = ?",
+            [userId.value]
+        )
+
+        if(rows.length === 0){
+            return NextResponse.json(
+                {message: "Akun tidak ditemukan"},
+                {status: 404}
+            )
+        }
+         
+        const user = rows[0]
+
+        return NextResponse.json({
+            id: user.id,
+            username: user.username,
+            password: user.password
+        })
+
+    } catch(error){
+        return NextResponse.json(
+            {message: "Server Error"},
+            {status: 500}
+        )
+    }
+}
---
+++ app/user-profile/page.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { useState, useEffect } from "react"
+import Image from "next/image"
+
+import showPass from "@/public/password/eye-alt-svgrepo-com.svg"
+import hidePass from "@/public/password/eye-slash-alt-svgrepo-com.svg"
+
+export default function UserProfile() {
+
+    const [showPassword, setShowPassword] = useState(false)
+    const [user, setUser] = useState<any>(null);
+
+    useEffect(() => {
+        getProfile()
+    }, [])
+
+    const getProfile = async () => {
+        const res = await fetch("/api/user-profile")
+        const data = await res.json()
+
+        console.log(data)
+
+        if(res.ok){
+            setUser(data)
+        } else{
+            alert(data.message)
+        }
+    }
+
+    return (
+        <section className="items-center flex justify-center min-h-screen bg-slate-100">
+            <div className="container mx-auto">
+                <div className="flex flex-col items-center justify-center">
+                    <div className="bg-white text-center xl:w-1/3 w-2/3 px-6 py-10 space-y-3 shadow-md">
+                        <h1 className="font-arial text-darkb font-semibold text-2xl">Akun Kamu</h1>
+                        <input value={user?.username || ""} readOnly className="border w-full p-2 rounded-md font-arial text-darkb focus:outline-0" placeholder="Nama Pengguna" autoComplete="off"
+                        />
+                        <div className="relative">
+                            <input value={user?.password || ""} readOnly type={showPassword ? "text" : "password"} className="border w-full p-2 rounded-md font-arial text-darkb focus:outline-0" placeholder="Kata Sandi" autoComplete="off" />
+                            <button type='button' onClick={() => setShowPassword(!showPassword)} className='absolute right-4 top-1/2 -translate-y-1/2'>
+                                <Image src={showPassword ? hidePass : showPass} alt={showPassword ? "Hide password" : "Show password"} width={20} height={20} />
+                            </button>
+                        </div>
+                        <button className="border rounded-md w-full py-1 font-arial bg-primary border-primary text-white hover:bg-sky-600 duration-200 cursor-pointer">Edit Akun</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
