c54c7101fbafd7c4e20bcffe2cdb394c51678ec7
ilhamhlmi <ilhamhilmi206@gmail.com>
2026-06-06 01:47:49 +0700

    recalling

--- app/api/list-product/[id]/route.ts
+++ app/api/list-product/[id]/route.ts
@@ -3,13 +3,16 @@ import { NextResponse } from "next/server";
 
 export async function GET(req: Request, { params }: any) {
     try {
+        // ini bikin variabel query SQL dulu
         const [rows]: any = await db.query(
             "SELECT product_name, stock, price FROM products WHERE id = ?",
             [params.id]
         )
 
+        // hasil query baris 1 akan dioper ke product
         const product = rows[0]
 
+        // conditional kalo productnya gaada
         if(product === undefined){
             return NextResponse.json(
                 {message: "Produk tidak ada"},
@@ -17,11 +20,13 @@ export async function GET(req: Request, { params }: any) {
             )
         }
 
+        // dibalikin ke fe, product = hasil query .nama-field
         return NextResponse.json({
             product_name: product.product_name,
             stock: product.stock,
             price: product.price
         })
+        
     } catch(error){
         return NextResponse.json(
             {message: "Server error"},
