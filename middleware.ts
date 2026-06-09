2c99ab9236e054e52c48bdf591bac7a8dfbf425e
ilhamhlmi <ilhamhilmi206@gmail.com>
2026-06-09 17:41:52 +0700

    middleware done

--- middleware.ts
+++ middleware.ts
@@ -7,5 +7,36 @@ export function middleware(request: NextRequest) {
     const pathname = request.nextUrl.pathname
 
     // protect page
-    const protectedRoutes = ["/inventory", "/user-profile", "/edit-profile", "/input-product"]
+    const protectedRoutes = [
+        "/inventory", 
+        "/user-profile", 
+        "/edit-profile", 
+        "/input-product",
+        "/edit-product"
+    ]
+
+    // route auth
+    const authRoutes = ["/login-operator", "/register-operator"]
+
+    if(!userId && protectedRoutes.includes(pathname)) {
+        return NextResponse.redirect(new URL("/login-operator", request.url))
+    }
+
+    // ini kalo udah login
+    if(userId && authRoutes.includes(pathname)){
+        return NextResponse.redirect(new URL("/inventory", request.url))
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: [
+        "/inventory",
+        "/user-profile",
+        "/edit-profile",
+        "/input-product",
+        "/login-operator",
+        "/register-operator"
+    ]
 }
