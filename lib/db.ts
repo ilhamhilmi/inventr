98d5602f2355feee4032190a92cccf9c05f05302
ilhamhlmi <ilhamhilmi206@gmail.com>
2026-06-12 21:32:38 +0700

    fix

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,8 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "belajar_auth"
 });
